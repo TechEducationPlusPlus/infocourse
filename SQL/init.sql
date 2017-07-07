7a43e5f75836aa97238860ed97052bcd28d395d9
Alex Tsvetanov <alex@tsalex.tk>
2017-07-07 21:10:20 +0100

    first commit

--- SQL/init.sql
+++ SQL/init.sql
@@ -191,69 +191,75 @@ CREATE TABLE `Videos` (
   `Source` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `Label` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `ExamLink` text COLLATE utf8mb4_unicode_ci,
-  `UniqueID` int(11) NOT NULL
+  `UniqueID` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (UniqueID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `Videos`
 --
 
-INSERT INTO `Videos` (`ID`, `Course`, `Link`, `Source`, `Label`, `ExamLink`, `UniqueID`) VALUES
-('Preparing...', 'PHP1', '/courses/PHP/videos/0', 'https://www.youtube.com/embed/heXKR7KDt-c', 'Author: Yasen Georgiev', '#', 1),
-('Basics', 'PHP1', '/courses/PHP/videos/1', 'https://www.youtube.com/embed/dztfsg2tQDs', 'Author: Yasen Georgiev', '#', 2),
-('Arrays&amp;Conditional Statements - Group A', 'PHP1', '/courses/PHP/videos/2', 'https://www.youtube.com/embed/rKdvAiYdh7I', 'Author: Yasen Georgiev', '#', 3),
-('Arrays&amp;Conditional Statements - Group B', 'PHP1', '/courses/PHP/videos/3', 'https://www.youtube.com/embed/OytP2CQWjvY', 'Author: Yasen Georgiev', '#', 4),
-('Loops<br>Group A', 'PHP1', '/courses/PHP/videos/4', 'https://www.youtube.com/embed/CTziiSKich8', 'Author: Yasen Georgiev', '#', 5),
-('Loops<br>Group B', 'PHP1', '/courses/PHP/videos/5', 'https://www.youtube.com/embed/zWdqNpzOPhw', 'Author: Yasen Georgiev', '#', 6),
-('Loops - Group C', 'PHP1', '/courses/PHP/videos/6', 'https://www.youtube.com/embed/2ldFC-senNE', 'Author: Yasen Georgiev', '#', 7),
-('Live Solving', 'PHP1', '/courses/PHP/videos/7', 'https://www.youtube.com/embed/_J8sFWQKIS4', 'Author: Yasen Georgiev', '#', 8),
-('Hosting. ICN.Bg', 'PHP1', '/courses/PHP/videos/8', 'https://www.youtube.com/embed/ynwybdVp6Nk', 'Author: Yasen Georgiev', '#', 9),
-('Forms<br>Group A', 'PHP1', '/courses/PHP/videos/9', 'https://www.youtube.com/embed/9mn_cnvdBVY', 'Author: Yasen Georgiev', '#', 10),
-('Forms<br>Group B', 'PHP1', '/courses/PHP/videos/10', 'https://www.youtube.com/embed/DeS7xOxywl4', 'Author: Yasen Georgiev', '#', 11),
-('Cookies. Sessions - Group C', 'PHP1', '/courses/PHP/videos/11', 'https://www.youtube.com/embed/9bOBM5Z_Nr0', 'Author: Yasen Georgiev', '#', 12),
-('Exercise<br>Part 1', 'PHP1', '/courses/PHP/videos/12', 'https://www.youtube.com/embed/OQoDRBdSiqc', 'Author: Yasen Georgiev', '#', 13),
-('Exercise<br>Part 2', 'PHP1', '/courses/PHP/videos/13', 'https://www.youtube.com/embed/AS3CUMmTqmI', 'Author: Yasen Georgiev', '#', 14),
-('DataBases<br><br><br>', 'PHP2', '/courses/PHP/videos/14', 'https://www.youtube.com/embed/UCabYNOFUqw', 'Author: Yasen Georgiev', '#', 15),
-('MySQL &amp; phpMyAdmin<br><br><br>', 'PHP2', '/courses/PHP/videos/15', 'https://www.youtube.com/embed/raDNElaW6F', 'Author: Yasen Georgiev', '#', 16),
-('MySQL @ PHP<br><br><br>', 'PHP2', '/courses/PHP/videos/16', 'https://www.youtube.com/embed/MMPMpQEuDT8', 'Author: Yasen Georgiev', '#', 17),
-('Functions. Good Practices<br><br>', 'PHP2', '/courses/PHP/videos/17', 'https://www.youtube.com/embed/2LdLm4oV1Lk', 'Author: Yasen Georgiev', '#', 18),
-('Functions<br><br><br>', 'PHP2', '/courses/PHP/videos/18', 'https://www.youtube.com/embed/Ulix9SAcTxU', 'Author: Yasen Georgiev', '#', 19),
-('OOP basics<br><br><br>', 'PHP2', '/courses/PHP/videos/19', 'https://www.youtube.com/embed/HHXmedW45ms', 'Author: Yasen Georgiev', '#', 20),
-('Inheritance, Magic Methods, Publicity.<br><br>', 'PHP2', '/courses/PHP/videos/20', 'https://www.youtube.com/embed/I4dWkBamRYs', 'Author: Yasen Georgiev', '#', 21),
-('Inheritance, Magic Methods, Publicity. - Group B', 'PHP2', '/courses/PHP/videos/21', 'https://www.youtube.com/embed/XUwyfQ7WTnI', 'Author: Yasen Georgiev', '#', 22),
-('<span style=\"font-size: 0.75em\">Autoloading, namespaces, parent::, Interfaces - Group A</span>', 'PHP2', '/courses/PHP/videos/22', 'https://www.youtube.com/embed/BHkIIzJFti8', 'Author: Yasen Georgiev', '#', 23),
-('<span style=\"font-size: 0.75em\">Autoloading, namespaces, parent::, Interfaces - Group B</span>', 'PHP2', '/courses/PHP/videos/23', 'https://www.youtube.com/embed/_FA0tuK78ik', 'Author: Yasen Georgiev', '#', 24),
-('Exceptions<br><br><br>', 'PHP2', '/courses/PHP/videos/24', 'https://www.youtube.com/embed/Av8VNukyj0U', 'Author: Yasen Georgiev', '#', 25),
-('MVC. Laravel<br><br><br>', 'PHP2', '/courses/PHP/videos/25', 'https://www.youtube.com/embed/t522p2BwZ_g', 'Author: Yasen Georgiev', '#', 26),
-('<span style=\"font-size: 0.69em\">Eloquent ORM. Routing. Middlewares. Views. Blade Templating</span>', 'PHP2', '/courses/PHP/videos/26', 'https://www.youtube.com/embed/fedrAcbCboI', 'Author: Yasen Georgiev', '#', 27),
-('Phonebook - Part 1<br><br><br>', 'PHP2', '/courses/PHP/videos/27', 'https://www.youtube.com/embed/ee-dFlCWKj4', 'Author: Yasen Georgiev', '#', 28),
-('Phonebook - Part 2<br><br><br>', 'PHP2', '/courses/PHP/videos/28', 'https://www.youtube.com/embed/XP3W5XqTrPM', 'Author: Yasen Georgiev', '#', 29),
-('Introduction to C++ (#<span class=\"nvote\" data-return=\"true\">TechEdu Course 1</span>)', 'C++1', '/courses/C++/videos/Introduction+to+C%2b%2b', 'https://www.youtube.com/embed/Ot3Uxh6bZxU', 'Alex Tsvetanov', '#', 30),
-('I/O Stream', 'C++1', '/courses/C++/videos/IO%20Stream', 'https://www.youtube.com/embed/phM0lHrmVnE', 'Alex Tsvetanov', 'https://atj.openjudge.tk/contests/1', 31),
-('Data Types and Variables', 'C++1', '/courses/C++/videos/DataTypesVariables', 'https://www.youtube.com/embed/QCvTRpruM8E', 'Alex Tsvetanov', '#', 32),
-('Overflow, underflow and casting', 'C++1', '/courses/C++/videos/OverUnderCast', 'https://www.youtube.com/embed/zWMbeBmz4EU', 'Alex Tsvetanov', '#', 33),
-('Pointers', 'C++1', '/courses/C++/videos/Pointers', 'https://www.youtube.com/embed/M51p8Rs_4ls', 'Alex Tsvetanov', '#', 34),
-('if-else', 'C++1', '/courses/C++/videos/if-else', 'https://www.youtube.com/embed/sPV2_eJtgmk', 'Evgeni Vasilev', '#', 35),
-('switch-case', 'C++1', '/courses/C++/videos/switch', 'https://www.youtube.com/embed/yBxR6FZKl4E', 'Evgeni Vasilev', '#', 36),
-('while loop', 'C++1', '/courses/C++/videos/while', 'https://www.youtube.com/embed/LFENULP3qeg', 'Evgeni Vasilev', '#', 37),
-('do-while loop', 'C++1', '/courses/C++/videos/do-while', 'https://www.youtube.com/embed/ji6xvgGmiKc', 'Evgeni Vasilev', '#', 38),
-('for loop', 'C++1', '/courses/C++/videos/for', 'https://www.youtube.com/embed/QQtZD08-15g', 'Evgeni Vasilev', '#', 39),
-('break, continue', 'C++1', '/courses/C++/videos/break_continue', 'https://www.youtube.com/embed/FaISNM23cW0', 'Evgeni Vasilev', '#', 40),
-('Sort function', 'C++2', '/courses/C++/videos/sort_func', 'https://www.youtube.com/embed/okiF-XsrSWY', 'Marin Shalamanov', '#', 41),
-('Binary search function', 'C++2', '/courses/C++/videos/binary_search_function', 'https://www.youtube.com/embed/slk-hXnrXF0', 'Marin Shalamanov', '#', 42),
-('lower/upper bound', 'C++2', '/courses/C++/videos/bound_functions', 'https://www.youtube.com/embed/NJJ3CEKHySU', 'Marin Shalamanov', '#', 43),
-('Cube root function', 'C++2', '/courses/C++/videos/cube_root', 'https://www.youtube.com/embed/BtjdITzbvmk', 'Marin Shalamanov', '#', 44),
-('Workshop - sysadmin', 'C++2', '/courses/C++/videos/workshop_sysadmin', 'https://www.youtube.com/embed/vl3cAW2Kq6w', 'Marin Shalamanov', '#', 45),
-('Counting sort', 'SortFind', '/courses/Algorithms/videos/counting_sort', 'https://www.youtube.com/embed/wAPHExxXDjo', 'Marin Shalamanov', '#', 46),
-('Binary search', 'SortFind', '/courses/Algorithms/videos/binary_search', 'https://www.youtube.com/embed/cwmdfliNj5E', 'Marin Shalamanov', '#', 47),
-('Introduction in Graphs', 'Graphs', '/courses/Algorithms/videos/Introduction_to_Graphs', 'https://www.youtube.com/embed/wXv3VEMzEBM', 'Marin Shalamanov', '#', 48),
-('List of neighbours', 'Graphs', '/courses/Algorithms/videos/List_of_neighbours', 'https://www.youtube.com/embed/xqRLrrxjM-U', 'Marin Shalamanov', '#', 49),
-('BFS', 'Graphs', '/courses/Algorithms/videos/BFS', 'https://www.youtube.com/embed/qax90KL-lMk', 'Marin Shalamanov', '#', 50),
-('Min path with BFS', 'Graphs', '/courses/Algorithms/videos/Min_path_in_Graph', 'https://www.youtube.com/embed/igAboBxH4YE', 'Marin Shalamanov', '#', 51),
-('Welcome to TechEdu++', 'Java1', '/courses/Java1/videos/welcome', 'https://www.youtube.com/embed/M3Mr20rfr64', 'Hristo Todorov', '#', 53),
-('Introduction to programming', 'Java1', '/courses/Java1/videos/intro', 'https://www.youtube.com/embed/MKk3b49f1w8', 'Hristo Todorov', '#', 54),
-('DFS', 'Graphs', '/courses/Algorithms/videos/DFS', 'https://www.youtube.com/embed/dSqZZqjoDjA', 'Marin Shalamanov', '#', 52);
+INSERT INTO `Videos` (`ID`, `Course`, `Link`, `Source`, `Label`, `ExamLink`) VALUES
+('Preparing...', 'PHP1', '/courses/PHP/videos/0', 'https://www.youtube.com/embed/heXKR7KDt-c', 'Author: Yasen Georgiev', '#'),
+('Basics', 'PHP1', '/courses/PHP/videos/1', 'https://www.youtube.com/embed/dztfsg2tQDs', 'Author: Yasen Georgiev', '#'),
+('Arrays&amp;Conditional Statements - Group A', 'PHP1', '/courses/PHP/videos/2', 'https://www.youtube.com/embed/rKdvAiYdh7I', 'Author: Yasen Georgiev', '#'),
+('Arrays&amp;Conditional Statements - Group B', 'PHP1', '/courses/PHP/videos/3', 'https://www.youtube.com/embed/OytP2CQWjvY', 'Author: Yasen Georgiev', '#'),
+('Loops<br>Group A', 'PHP1', '/courses/PHP/videos/4', 'https://www.youtube.com/embed/CTziiSKich8', 'Author: Yasen Georgiev', '#'),
+('Loops<br>Group B', 'PHP1', '/courses/PHP/videos/5', 'https://www.youtube.com/embed/zWdqNpzOPhw', 'Author: Yasen Georgiev', '#'),
+('Loops - Group C', 'PHP1', '/courses/PHP/videos/6', 'https://www.youtube.com/embed/2ldFC-senNE', 'Author: Yasen Georgiev', '#'),
+('Live Solving', 'PHP1', '/courses/PHP/videos/7', 'https://www.youtube.com/embed/_J8sFWQKIS4', 'Author: Yasen Georgiev', '#'),
+('Hosting. ICN.Bg', 'PHP1', '/courses/PHP/videos/8', 'https://www.youtube.com/embed/ynwybdVp6Nk', 'Author: Yasen Georgiev', '#'),
+('Forms<br>Group A', 'PHP1', '/courses/PHP/videos/9', 'https://www.youtube.com/embed/9mn_cnvdBVY', 'Author: Yasen Georgiev', '#'),
+('Forms<br>Group B', 'PHP1', '/courses/PHP/videos/10', 'https://www.youtube.com/embed/DeS7xOxywl4', 'Author: Yasen Georgiev', '#'),
+('Cookies. Sessions - Group C', 'PHP1', '/courses/PHP/videos/11', 'https://www.youtube.com/embed/9bOBM5Z_Nr0', 'Author: Yasen Georgiev', '#'),
+('Exercise<br>Part 1', 'PHP1', '/courses/PHP/videos/12', 'https://www.youtube.com/embed/OQoDRBdSiqc', 'Author: Yasen Georgiev', '#'),
+('Exercise<br>Part 2', 'PHP1', '/courses/PHP/videos/13', 'https://www.youtube.com/embed/AS3CUMmTqmI', 'Author: Yasen Georgiev', '#'),
+('DataBases<br><br><br>', 'PHP2', '/courses/PHP/videos/14', 'https://www.youtube.com/embed/UCabYNOFUqw', 'Author: Yasen Georgiev', '#'),
+('MySQL &amp; phpMyAdmin<br><br><br>', 'PHP2', '/courses/PHP/videos/15', 'https://www.youtube.com/embed/raDNElaW6F', 'Author: Yasen Georgiev', '#'),
+('MySQL @ PHP<br><br><br>', 'PHP2', '/courses/PHP/videos/16', 'https://www.youtube.com/embed/MMPMpQEuDT8', 'Author: Yasen Georgiev', '#'),
+('Functions. Good Practices<br><br>', 'PHP2', '/courses/PHP/videos/17', 'https://www.youtube.com/embed/2LdLm4oV1Lk', 'Author: Yasen Georgiev', '#'),
+('Functions<br><br><br>', 'PHP2', '/courses/PHP/videos/18', 'https://www.youtube.com/embed/Ulix9SAcTxU', 'Author: Yasen Georgiev', '#'),
+('OOP basics<br><br><br>', 'PHP2', '/courses/PHP/videos/19', 'https://www.youtube.com/embed/HHXmedW45ms', 'Author: Yasen Georgiev', '#'),
+('Inheritance, Magic Methods, Publicity.<br><br>', 'PHP2', '/courses/PHP/videos/20', 'https://www.youtube.com/embed/I4dWkBamRYs', 'Author: Yasen Georgiev', '#'),
+('Inheritance, Magic Methods, Publicity. - Group B', 'PHP2', '/courses/PHP/videos/21', 'https://www.youtube.com/embed/XUwyfQ7WTnI', 'Author: Yasen Georgiev', '#'),
+('<span style=\"font-size: 0.75em\">Autoloading, namespaces, parent::, Interfaces - Group A</span>', 'PHP2', '/courses/PHP/videos/22', 'https://www.youtube.com/embed/BHkIIzJFti8', 'Author: Yasen Georgiev', '#'),
+('<span style=\"font-size: 0.75em\">Autoloading, namespaces, parent::, Interfaces - Group B</span>', 'PHP2', '/courses/PHP/videos/23', 'https://www.youtube.com/embed/_FA0tuK78ik', 'Author: Yasen Georgiev', '#'),
+('Exceptions<br><br><br>', 'PHP2', '/courses/PHP/videos/24', 'https://www.youtube.com/embed/Av8VNukyj0U', 'Author: Yasen Georgiev', '#'),
+('MVC. Laravel<br><br><br>', 'PHP2', '/courses/PHP/videos/25', 'https://www.youtube.com/embed/t522p2BwZ_g', 'Author: Yasen Georgiev', '#'),
+('<span style=\"font-size: 0.69em\">Eloquent ORM. Routing. Middlewares. Views. Blade Templating</span>', 'PHP2', '/courses/PHP/videos/26', 'https://www.youtube.com/embed/fedrAcbCboI', 'Author: Yasen Georgiev', '#'),
+('Phonebook - Part 1<br><br><br>', 'PHP2', '/courses/PHP/videos/27', 'https://www.youtube.com/embed/ee-dFlCWKj4', 'Author: Yasen Georgiev', '#'),
+('Phonebook - Part 2<br><br><br>', 'PHP2', '/courses/PHP/videos/28', 'https://www.youtube.com/embed/XP3W5XqTrPM', 'Author: Yasen Georgiev', '#'),
+('Introduction to C++ (#<span class=\"nvote\" data-return=\"true\">TechEdu Course 1</span>)', 'C++1', '/courses/C++/videos/Introduction+to+C%2b%2b', 'https://www.youtube.com/embed/Ot3Uxh6bZxU', 'Alex Tsvetanov', '#'),
+('I/O Stream', 'C++1', '/courses/C++/videos/IO%20Stream', 'https://www.youtube.com/embed/phM0lHrmVnE', 'Alex Tsvetanov', 'https://atj.openjudge.tk/contests/1'),
+('Data Types and Variables', 'C++1', '/courses/C++/videos/DataTypesVariables', 'https://www.youtube.com/embed/QCvTRpruM8E', 'Alex Tsvetanov', '#'),
+('Overflow, underflow and casting', 'C++1', '/courses/C++/videos/OverUnderCast', 'https://www.youtube.com/embed/zWMbeBmz4EU', 'Alex Tsvetanov', '#'),
+('Pointers', 'C++1', '/courses/C++/videos/Pointers', 'https://www.youtube.com/embed/M51p8Rs_4ls', 'Alex Tsvetanov', '#'),
+('Sort function', 'C++2', '/courses/C++/videos/sort_func', 'https://www.youtube.com/embed/okiF-XsrSWY', 'Marin Shalamanov', '#'),
+('Binary search function', 'C++2', '/courses/C++/videos/binary_search_function', 'https://www.youtube.com/embed/slk-hXnrXF0', 'Marin Shalamanov', '#'),
+('lower/upper bound', 'C++2', '/courses/C++/videos/bound_functions', 'https://www.youtube.com/embed/NJJ3CEKHySU', 'Marin Shalamanov', '#'),
+('Cube root function', 'C++2', '/courses/C++/videos/cube_root', 'https://www.youtube.com/embed/BtjdITzbvmk', 'Marin Shalamanov', '#'),
+('Workshop - sysadmin', 'C++2', '/courses/C++/videos/workshop_sysadmin', 'https://www.youtube.com/embed/vl3cAW2Kq6w', 'Marin Shalamanov', '#'),
+('Counting sort', 'SortFind', '/courses/Algorithms/videos/counting_sort', 'https://www.youtube.com/embed/wAPHExxXDjo', 'Marin Shalamanov', '#'),
+('Binary search', 'SortFind', '/courses/Algorithms/videos/binary_search', 'https://www.youtube.com/embed/cwmdfliNj5E', 'Marin Shalamanov', '#'),
+('Introduction in Graphs', 'Graphs', '/courses/Algorithms/videos/Introduction_to_Graphs', 'https://www.youtube.com/embed/wXv3VEMzEBM', 'Marin Shalamanov', '#'),
+('List of neighbours', 'Graphs', '/courses/Algorithms/videos/List_of_neighbours', 'https://www.youtube.com/embed/xqRLrrxjM-U', 'Marin Shalamanov', '#'),
+('BFS', 'Graphs', '/courses/Algorithms/videos/BFS', 'https://www.youtube.com/embed/qax90KL-lMk', 'Marin Shalamanov', '#'),
+('Min path with BFS', 'Graphs', '/courses/Algorithms/videos/Min_path_in_Graph', 'https://www.youtube.com/embed/igAboBxH4YE', 'Marin Shalamanov', '#'),
+('Welcome to TechEdu++', 'Java1', '/courses/Java1/videos/welcome', 'https://www.youtube.com/embed/M3Mr20rfr64', 'Hristo Todorov', '#'),
+('Introduction to programming', 'Java1', '/courses/Java1/videos/intro', 'https://www.youtube.com/embed/MKk3b49f1w8', 'Hristo Todorov', '#'),
+('DFS', 'Graphs', '/courses/Algorithms/videos/DFS', 'https://www.youtube.com/embed/dSqZZqjoDjA', 'Marin Shalamanov', '#'),
+('Introduction to loops', 'C++1', '/courses/C++1/videos/LoopsInto', "https://www.youtube.com/embed/_4Zr66NdjKQ", 'Dobrin Bashev', '#'),
+('For, While, Do-while loops', 'C++1', '/courses/C++1/videos/LoopsOperators', "https://www.youtube.com/embed/LZLg2fxbw0s", 'Dobrin Bashev', '#'),
+('Nested loops, break/continue', 'C++1', '/courses/C++1/videos/NestedLoops', "https://youtu.be/tLhyoOlhC2E", 'Dobrin Bashev', '#'),
+('Loops workshop', 'C++1', '/courses/C++1/videos/LoopsWorkshop', "https://youtu.be/WhCTwXYOF3M", 'Dobrin Bashev', '#')
+;
 
+--('if-else', 'C++1', '/courses/C++/videos/if-else', 'https://www.youtube.com/embed/sPV2_eJtgmk', 'Evgeni Vasilev', '#'),
+--('switch-case', 'C++1', '/courses/C++/videos/switch', 'https://www.youtube.com/embed/yBxR6FZKl4E', 'Evgeni Vasilev', '#'),
+--('while loop', 'C++1', '/courses/C++/videos/while', 'https://www.youtube.com/embed/LFENULP3qeg', 'Evgeni Vasilev', '#'),
+--('do-while loop', 'C++1', '/courses/C++/videos/do-while', 'https://www.youtube.com/embed/ji6xvgGmiKc', 'Evgeni Vasilev', '#'),
+--('for loop', 'C++1', '/courses/C++/videos/for', 'https://www.youtube.com/embed/QQtZD08-15g', 'Evgeni Vasilev', '#'),
+--('break, continue', 'C++1', '/courses/C++/videos/break_continue', 'https://www.youtube.com/embed/FaISNM23cW0', 'Evgeni Vasilev', '#'),
 --
 -- Indexes for dumped tables
 --
@@ -297,8 +303,8 @@ ALTER TABLE `Users`
 --
 -- Indexes for table `Videos`
 --
-ALTER TABLE `Videos`
-  ADD PRIMARY KEY (`UniqueID`);
+--ALTER TABLE `Videos`
+--  ADD PRIMARY KEY (`UniqueID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
